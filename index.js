5491e0ee05a4aeff98c7bb13df84c0650f9457e8
Marcelo Viana <mviana2k@gmail.com>
2019-10-15 17:57:55 -0300

    rev2

--- index.js
+++ index.js
@@ -3,23 +3,59 @@
  */
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, AsyncStorage, Platform, Root } from 'react-native';
 import ReviewForm from './src/components/ReviewForm';
-import { name as appName } from './app.jason';
-import { Root } from 'native-base';
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { name as appName } from './app.json';
+import { createAppContainer } from "react-navigation";
+import {createBottomTabNavigator} from "react-navigation-tabs"
+import ReviewList from './src/components/ReviewList';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reviewsReducer from './src/reducers/ReviewsReducer';
+import Provider from 'react-redux';
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import PersistGate from 'redux-persist/lib/integration/react';
+import RemoteList from './src/components/RemoteList';
+import MessageService from './src/services/MessageService';
+
+
+const persistConfig = {
+    key: 'reviews',
+    storage: AsyncStorage
+};
+
+const rootReducer = combineReducers({ reviews: reviewsReducer });
+const persistentReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(
+    persistentReducer,
+    applyMiddleware(thunk)
+);
+
+const persistor = persistStore(store);
 
 const TabNavigator = createBottomTabNavigator({
-    ReviewForm
+    ReviewForm, ReviewList, RemoteList
 });
 const AppContainer = createAppContainer(TabNavigator);
 
+if (Platform.OS === 'android') {
+    MessageService.initToken().then(() => {
+        MessageService.listenMessages();
+    })
+}
+
 const wrappedView = () => {
     return (
-        <Root>
-                <AppContainer />
-        </Root>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Root>
+                    <AppContainer />
+                </Root>
+            </PersistGate>
+        </Provider>
     );
 }
 
 AppRegistry.registerComponent(appName, () => wrappedView);
+
+
